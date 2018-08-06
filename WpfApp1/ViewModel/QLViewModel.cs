4917c616b11919b940ed2efa509d56fb07f5c779
Eric <emartineau52@gmail.com>
2018-08-06 04:03:29 -0400

    Add ViewModel, wraps Lancher

---
+++ WpfApp1/ViewModel/QLViewModel.cs
@@ -0,0 +1,18 @@
+﻿using QuickLaunch.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickLaunch.ViewModel
+{
+    class QLViewModel
+    {
+        private Launcher launcher;
+
+        public Dictionary<string, Launchable> Launchables { get => launcher.launchables; set => launcher.launchables = value; }
+
+
+    }
+}
