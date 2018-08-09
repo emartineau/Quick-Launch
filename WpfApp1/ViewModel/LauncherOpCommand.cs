10505e6695d4bc3019e0deffad16e1e3c843fcb9
Eric <emartineau52@gmail.com>
2018-08-09 18:40:46 -0400

    Add LauncherOpCommand : ICommand

---
+++ WpfApp1/ViewModel/LauncherOpCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuickLaunch.ViewModel
+{
+    class LauncherOpCommand : ICommand
+    {
+        private QLViewModel QLViewModel;
+        private LauncherOps.LauncherOp LauncherOp;
+
+        public event EventHandler CanExecuteChanged;
+
+        public LauncherOpCommand(QLViewModel qlViewModel, LauncherOps.LauncherOp launcherOp)
+        {
+            QLViewModel = qlViewModel;
+            LauncherOp = launcherOp;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            LauncherOp(QLViewModel);
+        }
+    }
+}
