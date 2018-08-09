839f854809757507469c41278278f5e95bda28ba
Eric <emartineau52@gmail.com>
2018-08-09 18:51:26 -0400

    Add LanchCommand

---
+++ WpfApp1/ViewModel/LaunchCommand.cs
@@ -0,0 +1,32 @@
+﻿using QuickLaunch.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuickLaunch.ViewModel
+{
+    class LaunchCommand : ICommand
+    {
+        private Launchable Launchable;
+
+        public LaunchCommand(Launchable launchable)
+        {
+            Launchable = launchable;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true; //TODO: Block if launchable file/folder path does not currently exist.
+        }
+
+        public void Execute(object parameter)
+        {
+            Launchable.Start();
+        }
+    }
+}
